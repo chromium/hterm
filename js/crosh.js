0a70a45e13997341e141d71f316a828cc83d65ed
tbarzic <tbarzic@google.com>
2012-01-19 18:13:26 -0800

    Add Crosh Terminal code.
    The code is "dead" at the moment. I'm waiting for chrome side changes to land
    before we expose it. But, it has been tested with chrome chages in place.
    
    TEST=none
    BUG=chromium-os:23273
    
    Change-Id: I6d8a136fd392dd19570ba5c759a596e23c411608
    Reviewed-on: https://gerrit.chromium.org/gerrit/14543
    Reviewed-by: Robert Ginda <rginda@chromium.org>
    Commit-Ready: Toni Barzic <tbarzic@chromium.org>
    Tested-by: Toni Barzic <tbarzic@chromium.org>

---
+++ js/crosh.js
@@ -0,0 +1,127 @@
+// Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * The Crosh-powered terminal command.
+ *
+ * This class defines a command that can be run in an hterm.Terminal instance.
+ * The Crosh command uses terminalPrivate extension API to create and use crosh
+ * process on ChromeOS machine.
+ *
+ *
+ * @param {Object} argv The argument object passed in from the Terminal.
+ */
+hterm.Crosh = function(argv) {
+  this.argv_ = argv;
+  this.io = null;
+  this.pid_ = -1;
+};
+
+/**
+ * Static initialier called from crosh.html.
+ *
+ * This constructs a new Terminal instance and instructs it to run the Crosh
+ * command.
+ */
+hterm.Crosh.init = function() {
+  var terminal = new hterm.Terminal(15, 18);
+  terminal.decorate(document.querySelector('#terminal'));
+
+  // Useful for console debugging.
+  window.term_ = terminal;
+
+  setTimeout(function() {
+      terminal.setCursorPosition(0, 0);
+      terminal.setCursorVisible(true);
+      terminal.runCommandClass(hterm.Crosh, document.location.hash.substr(1));
+    }, 0);
+  return true;
+};
+
+/**
+ * The name of this command used in messages to the user.
+ *
+ * Perhaps this will also be used by the user to invoke this command, if we
+ * build a shell command.
+ */
+hterm.Crosh.prototype.commandName = 'crosh';
+
+/**
+ * Called when an event from the crosh process is detected.
+ *
+ * @param pid Process id of the process the event came from.
+ * @param type Type of the event.
+ *             'stdout': Process output detected.
+ *             'exit': Process has exited.
+ * @param text Text that was detected on process output.
+**/
+hterm.Crosh.prototype.onProcessOutput_ = function(pid, type, text) {
+  if (this.pid_ == -1 || pid != this.pid_)
+    return;
+
+  if (type == 'exit') {
+    this.exit(0);
+    return;
+  }
+
+  this.io.print(text);
+}
+
+/**
+ * Start the crosh command.
+ *
+ * This is invoked by the terminal as a result of terminal.runCommandClass().
+ */
+hterm.Crosh.prototype.run = function() {
+  this.io = this.argv_.io.push();
+  this.io.onVTKeystroke = this.sendString_.bind(this);
+  this.io.sendString = this.sendString_.bind(this);
+
+  var self = this;
+  chrome.terminalPrivate.onProcessOutput.addListener(
+      this.onProcessOutput_.bind(this));
+  document.body.onunload = this.close_.bind(this);
+  chrome.terminalPrivate.openTerminalProcess(this.commandName,
+      function(pid) {
+        if (pid == undefined || pid == -1) {
+          self.io.println("Opening crosh process failed.");
+          self.exit(1);
+          return;
+        }
+        self.pid_ = pid;
+      }
+  );
+};
+
+/**
+ * Send a string to the crosh process.
+ *
+ * @param {string} string The string to send.
+ */
+hterm.Crosh.prototype.sendString_ = function(string) {
+  if (this.pid_ == -1)
+    return;
+  chrome.terminalPrivate.sendInput(this.pid_, string);
+};
+
+/**
+ * Closes crosh terminal and exits the crosh command.
+**/
+hterm.Crosh.prototype.close_ = function() {
+    if (this.pid_ == -1)
+      return;
+    chrome.terminalPrivate.closeTerminalProcess(this.pid_);
+    this.pid_ = -1;
+}
+
+/**
+ * Exit the crosh command.
+ */
+hterm.Crosh.prototype.exit = function(code) {
+  this.close_();
+  this.io.pop();
+  if (this.argv_.onExit)
+    this.argv_.onExit(code);
+};
+
