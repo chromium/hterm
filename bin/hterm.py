73b7d9c7f8ed77be45649ba44b79121f7e429bbd
Mike Frysinger <vapier@chromium.org>
2020-05-14 01:21:02 -0400

    hterm/kokoro/nassh/ssh_client/terminal: convert core constants to pathlib
    We did this for libdot already, so convert these core constants over
    to the new pathlib API too.  This pathlib API is a bit nicer to work
    with and tends to take less code.
    
    Change-Id: Id694d697c421a8e44fff9ae2e0e3d95260a1f1c4
    Reviewed-on: https://chromium-review.googlesource.com/c/apps/libapps/+/2200249
    Tested-by: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>

--- bin/hterm.py
+++ bin/hterm.py
@@ -8,16 +8,16 @@
 
 from __future__ import print_function
 
-import os
+from pathlib import Path
 import sys
 
 
-BIN_DIR = os.path.dirname(os.path.realpath(__file__))
-DIR = os.path.dirname(BIN_DIR)
-LIBAPPS_DIR = os.path.dirname(DIR)
+BIN_DIR = Path(__file__).resolve().parent
+DIR = BIN_DIR.parent
+LIBAPPS_DIR = DIR.parent
 
 
-sys.path.insert(0, os.path.join(LIBAPPS_DIR, 'libdot', 'bin'))
+sys.path.insert(0, str(LIBAPPS_DIR / 'libdot' / 'bin'))
 
 # pylint: disable=unused-import
 import libdot  # pylint: disable=wrong-import-position
